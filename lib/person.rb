b855302912c2dafb28bbd97fadf4194217bb336f
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 03:16:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 # your code goes here
 class Person
+  attr_accessor :happienss, :hygiene
+  
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
-    
   end
   
   
